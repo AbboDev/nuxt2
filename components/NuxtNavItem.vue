62a3e44e8b09b06699bcd72a29fd81686bddebec
AbboDev <abbodev@gmail.com>
2023-01-02 18:20:22 +0100

    feat: add custom nuxt link based on ui-nav-item

---
+++ components/NuxtNavItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <NuxtLink :to="href" :class="className" @click="handleClick">
+    <span
+      v-if="hasRipple"
+      :class="deprecatedListClassNameMap['mdc-list-item__ripple']"
+    ></span>
+    <slot name="before" :icon-class="UI_ITEM.cssClasses.firstTile"></slot>
+    <slot>
+      <!-- text content -->
+    </slot>
+    <slot name="after" :icon-class="UI_ITEM.cssClasses.lastTile"></slot>
+  </NuxtLink>
+</template>
+
+<script>
+import UiNavItem from 'balm-ui/src/scripts/components/list/nav-item.vue'
+
+export default {
+  name: 'NuxtNavItem',
+  extends: UiNavItem,
+}
+</script>
