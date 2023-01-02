3b8c7e26f24415fcdfaf5da045102fbb357d4b97
AbboDev <abbodev@gmail.com>
2023-01-02 22:17:01 +0100

    feat: create preset password field

---
+++ components/PasswordField.vue
@@ -0,0 +1,49 @@
+<template>
+  <UiTextfield
+    outlined
+    required
+    pattern="[a-zA-Z0-9.@_-]{1,}"
+    helper-text-id="pw-validation-msg"
+    :input-type="isPasswordShowed ? 'text' : 'password'"
+    :attrs="{ autocomplete: 'current-password' }"
+    :model="value"
+    @input="handleInput"
+  >
+    <slot>Password</slot>
+
+    <template #after>
+      <UiTextfieldIcon @click="showPassword">{{
+        isPasswordShowed ? 'visibility_off' : 'visibility'
+      }}</UiTextfieldIcon>
+    </template>
+  </UiTextfield>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+
+    // isPasswordShowed: {
+    //   type: Boolean,
+    //   default: false,
+    // },
+  },
+  data() {
+    return {
+      isPasswordShowed: false,
+    }
+  },
+  methods: {
+    showPassword(): void {
+      this.isPasswordShowed = !this.isPasswordShowed
+    },
+    handleInput(newPassword: string): void {
+      this.$emit('input', newPassword)
+    },
+  },
+}
+</script>
