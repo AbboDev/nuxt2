ed51566ae7d7f6754ccfed83989f2ad4e2f53fa0
AbboDev <abbodev@gmail.com>
2023-01-02 23:22:35 +0100

    feat: add profile page

---
+++ pages/Profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="o-page">
+    <div class="o-page__content">
+      <LoginCard></LoginCard>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ProfilePage',
+}
+</script>
