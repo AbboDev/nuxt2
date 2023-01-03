24e31f2e6fb85715bb307f697424ec61b1425f0a
AbboDev <abbodev@gmail.com>
2023-01-04 00:53:17 +0100

    feat: setup single post page to handle 404

--- src/pages/posts/_id.vue
+++ src/pages/posts/_id.vue
@@ -13,11 +13,16 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'SinglePostPage',
   middleware: 'onlyLoggedIn',
-  async asyncData({ $axios, params }) {
-    const post: Post = await $axios.$get(
-      `https://dummyjson.com/posts/${params.id}`
-    )
-    return { post }
+  async asyncData({ $axios, params, error }) {
+    try {
+      const post: Post = await $axios.$get(
+        `https://dummyjson.com/posts/${params.id}`
+      )
+
+      return { post }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Post not found' })
+    }
   },
   data() {
     return {
