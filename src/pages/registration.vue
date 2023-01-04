7d363341cc37e60100cbfba9648153dfd580e36b
AbboDev <abbodev@gmail.com>
2023-01-04 11:17:24 +0100

    refactor: change all pages name convention

--- src/pages/registration.vue
+++ src/pages/registration.vue
@@ -1,23 +1,23 @@
 <template>
   <section class="o-page">
     <div class="o-page__content">
-      <LoginForm>
+      <RegisterForm>
         <UiDivider>o</UiDivider>
 
         <div>
-          Non sei ancora registrato?
-          <NuxtLink to="/registration">Registrati ora!</NuxtLink>
+          Hai già un account?
+          <NuxtLink to="/login">Effettua il login!</NuxtLink>
         </div>
-      </LoginForm>
+      </RegisterForm>
     </div>
   </section>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'LoginPage',
+  name: 'RegistrationPage',
   middleware: 'onlyGuest',
 })
 </script>
