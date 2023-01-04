e0d6ce57419445e0b5847440efcf94acd4ed1629
AbboDev <abbodev@gmail.com>
2023-01-04 17:55:25 +0100

    feat: add server search with debounce and pseudo autocomplete

--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -2,13 +2,24 @@
   <section>
     <h1>Tutti i post</h1>
 
-    <UiButton
-      :disabled="selectedRows.length === 0"
-      raised
-      :style="{ marginBottom: '16px' }"
-      @click="deletePosts"
-      >Elimina selezionati</UiButton
-    >
+    <div :style="{ marginBottom: '16px' }">
+      <UiButton
+        :disabled="selectedRows.length === 0"
+        raised
+        @click="deletePosts"
+        >Elimina selezionati</UiButton
+      >
+    </div>
+
+    <div :style="{ marginBottom: '16px' }">
+      <UiAutocomplete
+        v-model="search"
+        :source="source"
+        outlined
+        placeholder="Ricerca i termini"
+        @input="searchPosts"
+      ></UiAutocomplete>
+    </div>
 
     <UiTable
       v-model="selectedRows"
@@ -75,6 +86,8 @@ export default Vue.extend({
   middleware: 'onlyLoggedIn',
   data() {
     const page = this.$route.query?.page
+    const search = this.$route.query?.q || ''
+
     const limit: number = 30
 
     return {
@@ -135,25 +148,71 @@ export default Vue.extend({
       currentLimit: limit,
       limit,
       total: 0,
+      search,
+      source: [] as string[],
+      debounce: undefined as undefined | number,
     }
   },
   async fetch(): Promise<void> {
     const skip: number = this.limit * (this.page - 1)
-    const fetch = await this.$axios.$get<PostsPagination>(
-      `https://dummyjson.com/posts?limit=${this.limit}&skip=${skip}`
-    )
+    const searchParams: URLSearchParams = new URLSearchParams({
+      limit: (this.limit || 30).toString(),
+      skip: skip.toString(),
+    })
+
+    let url: URL = new URL(`https://dummyjson.com/posts/`)
+    let isFetchingWithSearch: boolean = false
+    if (typeof this.search === 'string' && this.search) {
+      isFetchingWithSearch = true
+      searchParams.set('q', this.search)
+      url = new URL('./search', url)
+    }
+
+    url = new URL(`${url}?${searchParams.toString()}`)
+
+    const fetch = await this.$axios.$get<PostsPagination>(url.toString())
 
     const { posts, total, limit } = fetch
+
+    if (this.posts) {
+      this.$toast({
+        timeoutMs: 2000,
+        message: isFetchingWithSearch
+          ? 'Nessun post trovato con la ricerca indicata'
+          : 'Oh no. Sembra che non sia stato ancora pubblicato alcun post',
+      })
+    }
 
     this.posts = posts
     this.total = total
     this.currentLimit = limit
     this.limit = limit
+
+    const source: string[] = this.posts.reduce<string[]>(
+      (accumulator, current: Post) => {
+        accumulator.push(...(current?.tags || []))
+        return accumulator
+      },
+      this.source
+    )
+
+    // Garantisce l'univocità dei risultati
+    this.source = [...new Set(source)]
   },
   watch: {
     '$route.query': function (query) {
+      let refresh: boolean = false
       if (query?.page && query.page !== this.page) {
         this.page = parseInt(query.page)
+        refresh = true
+      }
+
+      if (query?.q && query.q !== this.search) {
+        this.search = query.q
+        refresh = true
+      }
+
+      if (refresh) {
         this.$fetch()
       }
     },
@@ -185,8 +244,20 @@ export default Vue.extend({
     onPage(page: number): void {
       this.$router.push({
         path: this.$route.path,
-        query: { page: page.toString() },
+        query: { ...this.$route.query, page: page.toString() },
       })
+    },
+    searchPosts(text: string | number): void {
+      if (this.debounce) {
+        clearTimeout(this.debounce)
+      }
+
+      this.debounce = window.setTimeout(() => {
+        this.$router.push({
+          path: this.$route.path,
+          query: { ...this.$route.query, page: '1', q: text.toString() },
+        })
+      }, 250)
     },
     deletePosts(): void {
       if (this.selectedRows.length === 0) {
@@ -205,32 +276,32 @@ export default Vue.extend({
       return this.$axios
         .$delete(`https://dummyjson.com/posts/${post.id}`)
         .then(() => {
-          this.$toast({
-            message: 'Post eliminato con successo!',
-            className: 'is-success',
-          })
+        this.$toast({
+          message: 'Post eliminato con successo!',
+          className: 'is-success',
+        })
 
-          const postIndex: number = this.posts.indexOf(post)
-          if (postIndex > -1) {
-            this.posts.splice(postIndex, 1)
+        const postIndex: number = this.posts.indexOf(post)
+        if (postIndex > -1) {
+          this.posts.splice(postIndex, 1)
 
-            --this.currentLimit
-            --this.total
-          }
+          --this.currentLimit
+          --this.total
+        }
 
-          const index = this.selectedRows.indexOf(post.id)
-          if (index > -1) {
-            this.selectedRows.splice(index, 1)
-          }
+        const index = this.selectedRows.indexOf(post.id)
+        if (index > -1) {
+          this.selectedRows.splice(index, 1)
+        }
         })
         .catch((error: Error) => {
-          // eslint-disable-next-line no-console
-          console.error(error)
-          this.$toast({
-            message:
-              "È avvenuto un errore durante l'eliminazione del post. Riprova",
-            className: 'is-error',
-          })
+        // eslint-disable-next-line no-console
+        console.error(error)
+        this.$toast({
+          message:
+            "È avvenuto un errore durante l'eliminazione del post. Riprova",
+          className: 'is-error',
+        })
         })
     },
   },
@@ -238,6 +309,16 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+.mdc-autocomplete__menu {
+  max-width: 100%;
+  scrollbar-width: thin;
+
+  .mdc-deprecated-list-item {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+}
 .mdc-data-table {
   .mdc-data-table__cell {
     &.has-order {
