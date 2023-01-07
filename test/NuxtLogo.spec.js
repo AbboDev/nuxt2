85dcb3543981daa0d1e92f7430a4118bd4059a84
AbboDev <abbodev@gmail.com>
2023-01-07 23:43:58 +0100

    test: add first simple test

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -2,8 +2,17 @@ import { mount } from '@vue/test-utils'
 import NuxtLogo from '@/components/NuxtLogo.vue'
 
 describe('NuxtLogo', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(NuxtLogo)
+  })
+
   test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
     expect(wrapper.vm).toBeTruthy()
+  })
+
+  test('is a svg', () => {
+    expect(wrapper.element.tagName).toBe('svg')
   })
 })
